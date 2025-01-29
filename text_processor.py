9eeb5383725365db9c5cbcb651f91203faffe4c8
polirritmico <ebray187@gmail.com>
2025-01-28 23:46:46 -0300

    feat: change temp generated file to log

--- text_processor.py
+++ text_processor.py
@@ -14,6 +14,11 @@ SYMBOLS = {
     "—": "-",
     "«": '"',
     "»": '"',
+    ":": ".",  # Corrompe las frases
+    "¿": '"',
+    "?": '?"',
+    "¡": '"',
+    "!": '!"',
 }
 
 
@@ -21,6 +26,10 @@ def replace_unhandled_symbols(line: str) -> str:
     for target, replacement in SYMBOLS.items():
         line = line.replace(target, replacement)
     return line
+
+
+def remove_double_quotes(line: str) -> str:
+    return line.replace('""', '"')
 
 
 def add_missing_periods(line: str) -> str:
@@ -48,12 +57,33 @@ def uppercase_first_letter(line: str) -> str:
 
 
 def store_generated_text(text: str) -> None:
-    tmpdir = Path("tmp")
+    target_path = Path("logs")
     basename = "_text-processed_"
     basename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    tmpdir.mkdir(parents=True, exist_ok=True)
-    file = tmpdir / f"{basename}.txt"
+    target_path.mkdir(parents=True, exist_ok=True)
+    file = target_path / f"{basename}.txt"
     file.write_text(text, encoding="utf-8")
+
+
+def join_short_lines(text: list[str], min_width: int = 20) -> None:
+    prev_line: str | None = None
+    lines_to_remove = []
+    for line_number, line in enumerate(text):
+        if prev_line:
+            line = prev_line + " " + line
+            text[line_number] = line
+            prev_line = None
+
+        line_width = len(line)
+        if line_width <= min_width:
+            prev_line = line
+            lines_to_remove.append(line_number)
+
+    if prev_line:
+        text[-1] = prev_line + " " + text[-1]
+
+    for line_to_remove in reversed(lines_to_remove):
+        del text[line_to_remove]
 
 
 def text_processor(raw_text: str, replacement_dict: dict[str, str], opts: dict) -> str:
@@ -69,8 +99,11 @@ def text_processor(raw_text: str, replacement_dict: dict[str, str], opts: dict) 
         line = replace_unhandled_symbols(line)
         line = apply_custom_replacement_dict(line, replacement_dict)
         line = uppercase_first_letter(line)
+        line = remove_double_quotes(line)
 
         processed.append(line)
+
+    join_short_lines(processed, min_width=20)
 
     multiline_text = "\n\n".join(processed)
     store_generated_text(multiline_text)
